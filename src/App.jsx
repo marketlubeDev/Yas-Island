a0165881c5cf8958a8a551a926e59ef79b46dc1a
marketlubes <marketlubedevelopers@gmail.com>
2025-06-27 12:45:59 +0530

    Update navbar and product styles; replace background colors, adjust layout properties, and implement header in PaymentCheckout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
-import Footer from "./layouts/Footer/Footer";
 import Home from "./pages/Home/Home";
 import { useResponsive } from "./hooks/responsiveHook/useResponsive";
-import Header from "./layouts/Header/Header";
 import GlobalZoomEffect from "./components/GlobalZoomEffect";
 import GlobalInvertColor from "./components/GlobalInvertColor";
 import { useLanguage } from "./context/LanguageContext";
@@ -16,11 +14,9 @@ export default function App() {
     <div dir={isRTL ? "rtl" : "ltr"}>
       <GlobalZoomEffect />
       <GlobalInvertColor />
-      {/* <Header /> */}
       <Home>
         <Outlet />
       </Home>
-      {/* <Footer /> */}
     </div>
   );
 }
--- src/pages/PaymentCheckout/PaymentCheckout.jsx
+++ src/pages/PaymentCheckout/PaymentCheckout.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import PaymentCheckoutHeader from "./Components/PaymentCheckoutHeader";
 import PaymentCheckoutBody from "./Components/PaymentCheckoutBody";
+import Header from "../../layouts/Header/Header";
 
 export default function PaymentCheckout() {
   return (
     <div className="payment-checkout-page">
+      <Header />
       <PaymentCheckoutBody />
     </div>
   );
