8c811947ac1ee3f1541871aa3024b8346f164bfa
marketlubes@gmail.com <marketlubedevelopers@gmail.com>
2025-07-16 15:04:29 +0530

    Refactor ProductCard component: Enhance product mapping by adding index to keys for unique identification and improve code readability by removing unnecessary fragments.

--- src/pages/ProductPage/Components/ProductCard.jsx
+++ src/pages/ProductPage/Components/ProductCard.jsx
@@ -39,30 +39,38 @@ export default function ProductCard({ productList }) {
     return defaultVariant;
   };
 
+  console.log(productList, "proddsdsuctList");
+
   return (
     <div className="ProductCard">
       <div className="ProductCard__grid">
-        {productList?.map((product) => (
-          <div className="ProductCard__card" key={product?.product_title}>
-            <div className="ProductCard__card__image">
-              <img
-                src={product?.product_images?.thumbnail_url}
-                alt={product?.product_title}
+        {productList?.map((product, index) => (
+          <>
+            <div
+              className="ProductCard__card"
+              key={`${product?.product_title}-${index}`}
+            >
+              {/* <span>{index}</span> */}
+              <div className="ProductCard__card__image">
+                <img
+                  src={product?.product_images?.thumbnail_url}
+                  alt={product?.product_title}
+                />
+              </div>
+              <ProductCardContent
+                name={product?.product_title}
+                description={product?.productshortdesc}
+              />
+              <ProductCardPricetag
+                price={defaultVariant(product)?.gross}
+                tax={(defaultVariant(product)?.gross * 0.05).toFixed(2)}
+                currency={product?.currency}
+                taxDescription={product?.taxDescription}
+                onAddToCart={() => showModal(product)}
+                netPrice={defaultVariant(product)?.net_amount}
               />
             </div>
-            <ProductCardContent
-              name={product?.product_title}
-              description={product?.productshortdesc}
-            />
-            <ProductCardPricetag
-              price={defaultVariant(product)?.gross}
-              tax={(defaultVariant(product)?.gross * 0.05).toFixed(2)}
-              currency={product?.currency}
-              taxDescription={product?.taxDescription}
-              onAddToCart={() => showModal(product)}
-              netPrice={defaultVariant(product)?.net_amount}
-            />
-          </div>
+          </>
         ))}
       </div>
 
