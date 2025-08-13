c296c120c0832893245610229e29ff9845c2b3cf
Arjunliljo <arjun7180@gmail.com>
2025-08-13 17:39:11 +0530

    SL

--- src/layouts/SideBar/SideBar.jsx
+++ src/layouts/SideBar/SideBar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Logo from "../../components/Logo/Logo";
 import ChatWithUsButton from "../../components/buttons/ChatWithUsButton";
