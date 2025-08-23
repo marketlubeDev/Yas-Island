53d817883998a1a4aa726049600fd54dcb29631e
MarketLube Developers <marketlubedevelopers@gmail.com>
2025-08-23 17:04:37 +0530

    Enhance mobile responsiveness and layout for checkout components
    - Removed sticky positioning for checkout elements on mobile devices to improve usability.
    - Updated padding and margin styles across various screen sizes for better layout consistency.
    - Added media queries to adjust styles for different device breakpoints in checkout and payment components.
    - Refactored Header component to adjust mobile detection logic for improved rendering.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,12 @@ import LandingPage from "./LandingPage";
 import Footer from "../../layouts/Footer/Footer";
 import { useLocation } from "react-router-dom";
 import MobileHeader from "./MobileComponents/MobileHeader";
+import { useResponsive } from "../../hooks/responsiveHook/useResponsive";
 
 export default function Home({ children }) {
   const location = useLocation();
+  const { isSmallPhone, isPhone, isTablets, isBigTablets } = useResponsive();
+  const isMobile = isSmallPhone || isPhone || isTablets || isBigTablets;
   const shouldRenderGlobalFooter =
     location.pathname !== "/" &&
     location.pathname !== "/upcoming" &&
@@ -13,10 +16,11 @@ export default function Home({ children }) {
     location.pathname !== "/hotels" &&
     location.pathname !== "/dining" &&
     location.pathname !== "/live" &&
-    location.pathname !== "/shopping";
+    location.pathname !== "/shopping" &&
+    !isMobile;
 
   return (
-    <div className="home" style={{ height: "100vh" }}>
+    <div className="home" style={{ minHeight: isMobile ? "100vh" : "auto" }}>
       <MobileHeader />
       {/* <LandingPage /> */}
       {children}
