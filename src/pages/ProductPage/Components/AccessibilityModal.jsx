7c88b0964ae4c963aa73d63a0938869a843382b3
Arjunliljo <arjun7180@gmail.com>
2025-08-26 02:27:17 +0530

    LS

--- src/pages/ProductPage/Components/AccessibilityModal.jsx
+++ src/pages/ProductPage/Components/AccessibilityModal.jsx
@@ -38,7 +38,6 @@ export default function AccessibilityModal({ isOpen, onClose }) {
     { level: 1, label: "1x" },
     { level: 1.12, label: "1.25x" },
     { level: 1.25, label: "1.5x" },
-    { level: 1.5, label: "2x" },
   ];
 
   const handleDarkModeClick = () => {
