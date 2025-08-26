2434e902193b8570db64ae7ac8762aea3398de33
Arjunliljo <arjun7180@gmail.com>
2025-08-27 00:13:34 +0530

    LS

--- src/pages/PaymentCheckout/Components/OrderSummary.jsx
+++ src/pages/PaymentCheckout/Components/OrderSummary.jsx
@@ -486,22 +486,6 @@ export default function OrderSummary({
           </span>
         </div>
 
-        {/* Secure Payment Button - Mobile Style */}
-        {/* <div className="email-checkout__summary-securePayment">
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-          <path d="M9 12l2 2 4-4"></path>
-        </svg>
-        {t("orderSummary.securePayment")}
-      </div> */}
-
         <Modal
           open={isModalVisible}
           onOk={handleOk}
