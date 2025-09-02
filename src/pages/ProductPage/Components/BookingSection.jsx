dfdc3cfff164c437c96fd8e62fc236c59b607db0
marketlubes@gmail.com <marketlubedevelopers@gmail.com>
2025-09-02 12:01:09 +0530

    Refactor error handling in booking components to simplify toast messages. Removed redundant error message extraction and ensured consistent user feedback across BookingSection and BookingModalMbl components.

--- src/pages/ProductPage/Components/BookingSection.jsx
+++ src/pages/ProductPage/Components/BookingSection.jsx
@@ -158,12 +158,9 @@ export default function BookingSection({ product, onBack }) {
       }
     } catch (error) {
       console.log(error);
-      toast.error(
-        error?.response?.data?.message || t("toastMessages.somethingWentWrong"),
-        {
-          position: "top-center",
-        }
-      );
+      toast.error(t("toastMessages.somethingWentWrong"), {
+        position: "top-center",
+      });
       onBack();
     } finally {
       setIsLoadingDates(false);
