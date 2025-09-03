5b12a30ee22ac390bf6003a93358c2ea00215aef
MarketLube Developers <marketlubedevelopers@gmail.com>
2025-09-03 18:20:20 +0530

    Fix spacing in MycartMbl component for VAT and Tax display. Removed unnecessary colon for improved readability.

--- src/pages/Home/MobileComponents/MycartMbl.jsx
+++ src/pages/Home/MobileComponents/MycartMbl.jsx
@@ -382,7 +382,7 @@ function MycartMbl({ onClose, visible }) {
                     </span>
                   </div>
                   <div className="mycart-modal__summary-row">
-                    <span>{t("cart.vatAndTax")} :</span>
+                    <span>{t("cart.vatAndTax")} </span>
                     <span>
                       + {t("common.aed")} {vatAndTax.toFixed(2)}
                     </span>
