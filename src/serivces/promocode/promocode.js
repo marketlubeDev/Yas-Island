34e011cecfb173cb34e4adfd2e621fba2a0b1c37
Arjunliljo <arjun7180@gmail.com>
2025-09-10 05:47:12 +0530

    updated promocode endpoint

--- src/serivces/promocode/promocode.js
+++ src/serivces/promocode/promocode.js
@@ -1,9 +1,16 @@
 import apiClient from "../../../config/axiosInstance";
 
 const validatePromocode = async (promocode) => {
-  const response = await apiClient.get(
-    `/orders/validatepromocode?promocode=${promocode}`
+  const response = await apiClient.post(
+    `/products/validatepromocode`,
+    JSON.stringify(promocode),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
   );
+
   return response;
 };
 
