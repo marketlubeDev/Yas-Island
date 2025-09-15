e77c16952501f51f9918cbe8c6d2309f1f8af546
Ajay-Dev99 <ajaydevkomath@gmail.com>
2025-09-15 11:25:39 +0530

    Remove Arabic and English translation files along with the translation index to streamline localization setup.

--- src/translations/index.js
+++
@@ -1,9 +0,0 @@
-// Simple translation index
-import { loadTranslations } from "../utils/translationLoader";
-
-export const translations = {
-  English: "en",
-  العربية: "ar",
-};
-
-export { loadTranslations };
