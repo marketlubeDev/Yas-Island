e522ab22cd82914e80b20ff836088df624a3831f
Arjunliljo <arjun7180@gmail.com>
2025-09-18 11:37:47 +0530

    LS

--- src/pages/CardPayment/Desktop/Components/CardPaymentDetail.jsx
+++ src/pages/CardPayment/Desktop/Components/CardPaymentDetail.jsx
@@ -125,11 +125,9 @@ export default function CardPaymentDetail({ orderData, onBack }) {
       form.style.display = "none";
       form.id = "payfort-form";
 
-      console.log(orderData.tokenizationResponse, "formParameters");
-
       const parameters = {
         ...orderData.tokenizationResponse.formParameters,
-        language: currentLanguage,
+        // language: currentLanguage,
       };
 
       Object.entries(parameters || {}).forEach(([key, value]) => {
