4dc45f3be0c77026d4c846ccde0b201a01365d7a
marketlubedevelopers <marketlubes@gmail.com>
2025-09-20 10:20:56 +0530

    Enhance Product Card styling and content display
    - Implemented line clamping in the product card styles to limit text to two lines, ensuring a consistent layout.
    - Removed the previous description truncation logic in the ProductCardContent component to display the full description.
    - Updated CSS styles to maintain visual consistency across components.

--- src/pages/ProductPage/Components/ProductCardContent.jsx
+++ src/pages/ProductPage/Components/ProductCardContent.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 
 export default function ProductCardContent({ name, description }) {
-  const truncatedDescription =
-    description?.length > 100 ? `${description.slice(0, 55)}...` : description;
-
   return (
     <div className="ProductCard__card__content">
       <div
         className="ProductCard__card__content__title"
         dangerouslySetInnerHTML={{ __html: name }}
       ></div>
-      <p className="ProductCard__card__content__description">
-        {truncatedDescription}
-      </p>
+      <p className="ProductCard__card__content__description">{description}</p>
       <div className="ProductCard__card__content__divider"></div>
     </div>
   );
